844fe16e8bb00bc64226fe49810617d2654bcdff
Dan Aprahamian <dan.aprahamian@gmail.com>
2018-06-19 13:43:43 -0400

    foo

--- .evergreen/run-atlas-tests.sh
+++ .evergreen/run-atlas-tests.sh
@@ -2,9 +2,9 @@
 
 set -o errexit  # Exit the script with error if any of the commands fail
 
-export PATH="/opt/mongodbtoolchain/v2/bin:$PATH"
+export PROJECT_DIRECTORY="$(pwd)"
 NODE_ARTIFACTS_PATH="${PROJECT_DIRECTORY}/node-artifacts"
 export NVM_DIR="${NODE_ARTIFACTS_PATH}/nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-ATLAS_REPL=$ATLAS_REPL ATLAS_SHRD=$ATLAS_SHRD ATLAS_FREE=$ATLAS_FREE npm run atlas
+ATLAS_REPL="$ATLAS_REPL" ATLAS_SHRD="$ATLAS_SHRD" ATLAS_FREE="$ATLAS_FREE" npm run atlas
--- atlas.js
+++ atlas.js
@@ -1,29 +1,36 @@
 'use strict';
 
+console.log('starting atlas connectivity tests');
+
 const Core = require('./index');
 const parseConnectionString = Core.parseConnectionString;
+
+if (!(process.env.ATLAS_FREE && process.env.ATLAS_REPL && process.env.ATLAS_SHRD)) {
+  console.log('Error: Paths for Atlas tests not found');
+  process.exit(1);
+}
 
 const configs = [
   {
     name: 'ATLAS_REPL',
     url: process.env.ATLAS_REPL,
-    Ctor:  Core.ReplSet
+    Ctor: Core.ReplSet
   },
   {
     name: 'ATLAS_SHRD',
     url: process.env.ATLAS_SHRD,
-    Ctor:  Core.Mongos
+    Ctor: Core.Mongos
   },
   {
     name: 'ATLAS_FREE',
     url: process.env.ATLAS_FREE,
-    Ctor:  Core.ReplSet
+    Ctor: Core.ReplSet
   }
-]
+];
 
 function parse(url) {
   return new Promise((resolve, reject) => {
-    parseConnectionString(url, (err, r) => err ? reject(err) : resolve(r));
+    parseConnectionString(url, (err, r) => (err ? reject(err) : resolve(r)));
   });
 }
 
@@ -33,7 +40,13 @@ function topologyConnect(topology, auth) {
       if (!auth) {
         return resolve();
       }
-      topology.auth('default', 'admin', auth.username, auth.password, err => err ? reject(err) : resolve());
+      topology.auth(
+        'default',
+        'admin',
+        auth.username,
+        auth.password,
+        err => (err ? reject(err) : resolve())
+      );
     });
     topology.on('error', reject);
     topology.connect();
@@ -54,7 +67,7 @@ function topologyFindCmd(topology) {
 
 function topologyRunCmd(topology, ns, cmd) {
   return new Promise((resolve, reject) => {
-    topology.command(ns, cmd, {}, (err, r) => err ? reject(err) : resolve(r));
+    topology.command(ns, cmd, {}, (err, r) => (err ? reject(err) : resolve(r)));
   });
 }
 
@@ -62,14 +75,17 @@ function runConnectionTest(config) {
   let data;
   let topology;
   return Promise.resolve()
-    .then(() => parse(config.url)).then(_data => (data = _data))
-    .then(() => new config.Ctor(data.hosts, data.options)).then(_topology => (topology = _topology))
+    .then(() => parse(config.url))
+    .then(_data => (data = _data))
+    .then(() => new config.Ctor(data.hosts, data.options))
+    .then(_topology => (topology = _topology))
     .then(() => topologyConnect(topology, data.auth))
     .then(() => topologyIsMaster(topology))
-    .then(() => topologyFindCmd(topology))
+    .then(() => topologyFindCmd(topology));
 }
 
-configs.reduce((p, config) => p.then(() => runConnectionTest(config)), Promise.resolve())
+configs
+  .reduce((p, config) => p.then(() => runConnectionTest(config)), Promise.resolve())
   .then(() => {
     console.log('all tests passed');
     process.exit(0);
